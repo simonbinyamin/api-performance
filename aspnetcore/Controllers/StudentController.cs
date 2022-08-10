183265185e0b398afa1d7afb6c4f10cced298b43
Simon <Simon@DESKTOP-VJANTLS>
2022-08-10 14:12:52 +0100

    Add project files.

---
+++ aspnetcore/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+
+        [HttpGet("GetStudents")]
+        public string GetStudents()
+        {
+            return "sdad";
+
+        }
+
+        [HttpGet("FindText")]
+        public string FindText()
+        {
+            return "sdad";
+
+        }
+
+        [HttpGet("ZipFiles")]
+        public string ZipFiles()
+        {
+            return "sdad";
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ aspnetframework/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace aspnetframework.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
